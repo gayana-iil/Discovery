ec31acfec60f1cdd6e7f3154bf7a10ad08f7600b
prabhaship-iil <prabhashi.p@informaticsint.com>
2021-08-04 10:54:51 +0530

    Discovery Service Configuration

---
+++ discovery-service/src/test/java/com/informaticsint/infoins/discovery/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informaticsint.infoins.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
